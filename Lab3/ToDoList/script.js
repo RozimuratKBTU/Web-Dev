1c94b83710d33bdf778b4d99ef93eeba41f8c97c
Rozimurat <mirpulatovrozi11@gmail.com>
2024-02-13 23:39:25 +0600

    To Do List

---
+++ Lab3/ToDoList/script.js
@@ -0,0 +1,23 @@
+function addTask() {
+    var taskInput = document.getElementById('new-task');
+    var taskList = document.getElementById('tasks');
+
+
+    var li = document.createElement('li');
+    li.innerHTML =  taskInput.value +  ' <button class="delete-btn" onclick="deleteTask(this)">Delete</button>';
+
+
+
+    if(taskInput.value.trim() !== '') {
+        taskList.append(li);
+        taskInput.value = '';
+    } else {
+        alert('Please enter a task.');
+    }
+}
+
+function deleteTask(btn) {
+
+    var li = btn.parentNode;
+    li.parentNode.removeChild(li);
+}
