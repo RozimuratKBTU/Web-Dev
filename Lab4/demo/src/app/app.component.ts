32e63c0f229f3b66ac3840b632d49a1afaa96de4
Rozimurat <mirpulatovrozi11@gmail.com>
2024-02-21 23:25:38 +0600

    Add files via upload

--- Lab4/demo/src/app/app.component.ts
+++ Lab4/demo/src/app/app.component.ts
@@ -1,13 +1,19 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'demo';
+  title: String = 'My_First_App';
+
+
 }
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
