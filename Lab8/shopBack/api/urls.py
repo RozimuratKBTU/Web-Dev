c44e7628d343753d98eb1dfef3b0ed3106757f39
Rozimurat <mirpulatovrozi11@gmail.com>
2024-04-10 23:53:39 +0500

    initial commit

--- Lab8/shopBack/api/urls.py
+++ Lab8/shopBack/api/urls.py
@@ -3,4 +3,15 @@ from . import views
 
 urlpatterns = [
     path('products/', views.all_products),
+    path('products/<int:product_id>/', views.product, name='product'),
+    path('categories/', views.all_categories, name='category'),
+    path('categories/<int:category_id>/', views.category, name="categore"),
+    path('categories/<int:id>/products/', views.products_by_category)
 ]
+
+
+# /api/products - List of all Products
+# /api/products/<int:id>/ - Get one Product
+# /api/categories/ - List of all Categories
+# /api/categories/<int:id>/ - Get one Category
+# /api/categories/<int:id>/products/ - List of Products by Category
--- Lab8/shopBack/api/views.py
+++ Lab8/shopBack/api/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Product
+from .models import *
 from django.http.response import JsonResponse, HttpResponse
 from django.db import  models
 
@@ -13,5 +13,28 @@ def all_products(request):
     products_json = [p.to_json() for p in products]
     return JsonResponse(products_json, safe=False)
 
+def product(request, product_id):
+    try:
+        p = Product.objects.get(id=product_id)
+    except Product.DoesNotExist as error:
+        return JsonResponse({f'message: {error}'})
+
+    return JsonResponse(p.to_json(), safe=False)
+
+def all_categories(request):
+    categories = Category.objects.all()
+    categories_json = [ x.to_json() for x in categories]
+    return JsonResponse(categories_json, safe=False)
 
 
+def category(request, category_id):
+    try:
+        c = Category.objects.get(id = category_id)
+    except Category.DoesNotExist as e:
+        return JsonResponse({f'message: {e}'})
+    return JsonResponse(c.to_json(), safe=False)
+
+def products_by_category(request, category_id):
+    products = Product.objects.all()
+    products_json = [p.to_json() for p in products if p.category.id == category_id]
+    return JsonResponse(products_json,safe=False)
